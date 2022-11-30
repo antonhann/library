233713c6cfa2a13df76667b1d33cf5875fa88f3b
antonhann <antonha016@gmail.com>
2022-11-29 21:25:18 -0500

    fixed bugs problem with form old data

--- main.js
+++ main.js
@@ -1,70 +1,82 @@
-let collection = []
+let myLibrary = []
+let count = 0
 let section = ['title','author', 'pages','read']
-function Book(t,a,p,r){
-    this.title = t
-    this.author = a
-    this.pages = p
-    this.read = r
+
+class Book {
+    constructor(title, author, pages, read, id){
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read
+        this.id = id
+    }
+    changeRead(){
+        var readHTML = document.querySelector('.read' +this.id)
+        if(this.read == true){
+            this.read = false
+            
+        }else{
+            this.read = true
+        }
+        readHTML.innerHTML = 'read: ' + this.read
+    }
+    removeBook(){
+        myLibrary.splice(this.id, 1)
+    }
 }
+
+var div = document.querySelector('.myLibrary')
 var submit = document.querySelector('.display')
 var form = document.querySelector('.popup')
-submit.addEventListener('click', () => {    
+submit.addEventListener('click', () => { 
+    //form popus
     form.style.display = 'block'
     submit.style.display = 'none'
 });
+
 function addBook(){
+    //form pops back
     form.style.display = 'none'
     submit.style.display = 'block'
-    var a = document.querySelector('#title').value
-    var b = document.querySelector('#author').value
-    var c = document.querySelector('#page').value
-    var d = document.querySelector('#read').checked
-    var book = new Book(a, b, c, d)
-    collection.push(book)
+
+    //stores the user's data
+    var title = form.querySelector('#title').value
+    var author = form.querySelector('#author').value
+    var pages = form.querySelector('#page').value
+    var readStatus = form.querySelector('#read').checked
+    var book = new Book(title, author, pages, readStatus, count)
+    myLibrary.push(book)
+
+    //creates card to display the book
     var card = document.createElement('div')
     card.classList.add('card')
+    
+    //iterate through the form options and displays it on the card
     for (i in section){
         let key = book[section[i]]
         const text = document.createElement('p')
-        text.classList.add(section[i])
+        text.classList.add(section[i] + count)
         text.textContent = section[i] + ': '+ key
         card.appendChild(text)
     }
-    var r = document.createElement('button')
-    var remove = document.createElement('button')
-    r.innerHTML = 'READ?'
-    r.addEventListener('click', function(){
-        var read = document.querySelector('.read')
-        if (read.innerHTML == 'read: true'){
-            read.innerHTML = 'read: false'
-            book['read'] = false
-        }else{
-            read.innerHTML = 'read: true'
-            book['read'] = true
-        }
-    })
-    remove.innerHTML = 'REMOVE?'
-    remove.setAttribute('onclick','this.parentNode.parentNode.removeChild(this.parentNode);')
-    remove.style.marginLeft = '10px'
-    card.appendChild(r)
-    card.appendChild(remove)
-    div.appendChild(card)
-}
 
-var div = document.querySelector('.collection')
-for (let i in collection){
-    var card = document.createElement('div')
-    card.classList.add('card')
-    for (let j in section){
-        let key = i[j]
-        const text = document.createElement('p')
-        text.textContent = j + ': '+ key
-        card.appendChild(text)
-    }
-    var r = document.createElement('button')
-    var remove = document.createElement('button')
-    r.setAttribute('onclick','readtoggle()')
-    remove.setAttribute('onclick','this.parentNode.parentNode.removeChild(this.parentNode);')
-    card.appendChild(r)
+    //my change readButton
+    var readButton = document.createElement('button')
+    readButton.innerHTML = 'READ?'
+    readButton.addEventListener('click', () => {
+        book.changeRead()   
+    });
+
+    //remove book button
+    var removeButton = document.createElement('button')
+    removeButton.innerHTML = 'REMOVE?'
+    removeButton.setAttribute('onclick','this.parentNode.parentNode.removeChild(this.parentNode);')
+    removeButton.addEventListener('click', ()=>{
+        book.removeBook()
+    })
+
+    //finally adding the buttons to the card
+    card.appendChild(readButton)
+    card.appendChild(remove)
     div.appendChild(card)
 }
